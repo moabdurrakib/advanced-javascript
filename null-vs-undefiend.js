8d8173ae3f1dfb63b9550888d7bcd79b952a7e14
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-11-26 17:43:48 +0600

    null vs undefined for interview

---
+++ null-vs-undefiend.js
@@ -0,0 +1,17 @@
+let student;
+function add(num1,num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1, num2 );
+};
+
+const fruits = {name: "Mango", color:"Green"};
+console.log(furits.country);
+
+let fun = undefined;
+
+let ages = [2,7,30,];
+console.log(ages[34]);
