9f9d317f65a1db18515145ef10053e6195b9216b
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-11-26 21:27:30 +0600

    Map filter

---
+++ map-filter.js
@@ -0,0 +1,9 @@
+const number =[2,5,79,67,28,2];
+
+// const result = number.map(x => x * x);
+// console.log(result);
+// const bigger = number.filter(x => x > 5);
+// console.log(bigger);
+
+const finder = number.find( x => x > 58);
+console.log(finder);
