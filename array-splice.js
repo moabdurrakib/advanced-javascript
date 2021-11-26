4f8b5b65859c9553951016c1c5082db8f9e19133
mabdurrakib <mabdurrakib.dev@gmail.com>
2021-11-26 22:35:32 +0600

    array slice,splice and join

---
+++ array-splice.js
@@ -0,0 +1,11 @@
+const numbers = [12,37,83,23,90,12,];
+const parts = numbers.slice(3,8);
+console.log(parts);
+
+const part2 = numbers.splice(3,7); 
+
+console.log(part2);
+console.log(numbers);
+const numbJoin =  numbers.join("theke");
+console.log(numbJoin);
+
